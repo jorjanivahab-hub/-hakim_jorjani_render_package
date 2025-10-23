db72417cd99e631ebd030969b195226d94b05e9e
jorjanivahab-hub <jorjani.vahab@gmail.com>
2025-10-23 20:23:39 +0330

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,8 +1,6 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('courses/', views.courses, name='courses'),
-    path('register/', views.register, name='register'),
-    path('contact/', views.contact, name='contact'),
+    path('admin/', admin.site.urls),
+    path('', include('main.urls')),
 ]
